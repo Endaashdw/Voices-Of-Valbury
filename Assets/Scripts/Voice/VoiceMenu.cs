357842f3efcf62b06f2460082a3b84eda05f7a27
Pasas1345 <jamesban31@gmail.com>
2025-07-30 00:34:01 +0800

    added voice recognition for main menu

--- Assets/Scripts/Voice/VoiceMenu.cs
+++ Assets/Scripts/Voice/VoiceMenu.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 
-public class VoiceRecognition : MonoBehaviour
+public class VoiceMenu : MonoBehaviour
 {
-	public string[] keywords = { "boom", "hello" };
+	public string[] keywords = { "play" };
 	public ConfidenceLevel confidence = ConfidenceLevel.High;
-	protected string word = "boom";
+	protected string word = "play";
+	[SerializeField] private SaveManager saveManager;
+	[SerializeField] private SceneController sceneController;
 	KeywordRecognizer recognizer;
 
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,7 +18,10 @@ public class VoiceRecognition : MonoBehaviour
 	}
 
 	private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args) {
-		Debug.Log(args.text);
+		if (args.text == "play") {
+			saveManager.IncreaseAttempts();
+			sceneController.SceneChange("Test");
+		}
 	}
 
 	private void OnApplicationQuit() {
